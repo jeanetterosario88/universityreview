d6ccfb1341dc835c718ba35ddd8dd5dbf33be6ce
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-09-20 21:49:12 -0400

    University Controller Create method

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -8,4 +8,22 @@ class UniversitiesController < ApplicationController
         @university = University.new
     end
 
+    def create
+        university = University.create(university_params)
+        redirect_to university_path
+    end
+
+    def university_params
+        params.require(:university).permit(
+          :name,
+          addresses_attributes: [
+            :street_address_1,
+            :street_address_2,
+            :city,
+            :state,
+            :zipcode,
+          ]
+        )
+      end
+
 end
