ad229896763ff00590a50efec5f3655dde8a2c4c
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-09-23 11:09:35 -0400

    adding to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
 
     def index
-        puts University.count
-        puts University.joins(:reviews).count
+        # puts University.count
+        # puts University.joins(:reviews).count
         @universities = University.joins(:reviews).distinct.sort_by do |university|
             university.average_score
         end.reverse.first(3)
