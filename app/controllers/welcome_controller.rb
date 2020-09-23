ccd2733c4a6bfec895bb01a3601585dcd5e64ba2
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-09-22 22:01:19 -0400

    welcome page with top rated universities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
 
+
     def index
-       
+        puts University.count
+        puts University.joins(:reviews).count
+        @universities = University.joins(:reviews).distinct.sort_by do |university|
+            university.average_score
+        end.reverse.first(3)
     end
 
 end
--- app/models/university.rb
+++ app/models/university.rb
@@ -2,15 +2,14 @@ class University < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
 
-    validates :name, presence: true, length: { minimum: 3 } 
+    validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
     validates :address, presence: true, length: { is: 5 } 
 
-    def self.average_score
-        score_array = @university.reviews.collect { |review| review.score }
-        score_sum = score_array.sum
-        score_average = score_sum / score_array.size
+    def average_score
+        reviews.collect { |review| review.score }.sum.to_f / reviews.size
     end
 
+    
     # def self.by_score
     #     @universities.sort! { |a,b| a.average_score <=> b.average_score}
     # end
